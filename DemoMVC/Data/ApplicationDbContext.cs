4d3e624f959c0a722b38d98992e320f99c2b79cb
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2022-01-25 10:56:11 -0500

    Ajout des connections string MSSQL et LocalDB

--- DemoMVC/Data/ApplicationDbContext.cs
+++ DemoMVC/Data/ApplicationDbContext.cs
@@ -5,9 +5,10 @@ namespace DemoMVC.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Client> Clients { get; set; }
 
-        public DbSet<Inventaire> Inventaire { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
 
         public DbSet<Marque> Marques { get; set; }
 
---
+++ DemoMVC/Migrations/20220125155251_InitialMigration.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoMVC.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Argent = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marques",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marques", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modeles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MarqueId = table.Column<int>(type: "int", nullable: true),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modeles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modeles_Marques_MarqueId",
+                        column: x => x.MarqueId,
+                        principalTable: "Marques",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voitures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MarqueId = table.Column<int>(type: "int", nullable: true),
+                    ModeleId = table.Column<int>(type: "int", nullable: true),
+                    Prix = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voitures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Voitures_Marques_MarqueId",
+                        column: x => x.MarqueId,
+                        principalTable: "Marques",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Voitures_Modeles_ModeleId",
+                        column: x => x.ModeleId,
+                        principalTable: "Modeles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modeles_MarqueId",
+                table: "Modeles",
+                column: "MarqueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voitures_MarqueId",
+                table: "Voitures",
+                column: "MarqueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voitures_ModeleId",
+                table: "Voitures",
+                column: "ModeleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Voitures");
+
+            migrationBuilder.DropTable(
+                name: "Modeles");
+
+            migrationBuilder.DropTable(
+                name: "Marques");
+        }
+    }
+}
