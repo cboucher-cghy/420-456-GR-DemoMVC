56d9946b43a7755e524b114b820266eef76df1bf
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2022-04-06 12:05:27 -0400

    Ajout Demo Identity

---
+++ DemoMVC_Identity/Data/Migrations/20220401134352_ASPNET_Identity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoMVC_Identity.Data.Migrations
+{
+    public partial class ASPNET_Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
