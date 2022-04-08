cb53cbc083a065f53990280d43bd78aa67072ab4
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2022-04-08 14:06:10 -0400

    Ajout du seed pour un utilisateur

--- DemoMVC_Identity/Data/ApplicationDbContext.cs
+++ DemoMVC_Identity/Data/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 namespace DemoMVC_Identity.Data
 {
@@ -27,18 +26,43 @@ namespace DemoMVC_Identity.Data
             //    entity.ToTable(name: "Roles");
             //});
 
+            const string USER_ADMIN_ID = "02174cf0–9412–4cfe-afbf-59f706d72cf6";
+            const string ROLE_ID = "4ce0a981-6965-4a1e-9b36-f330ea2651f7";
+
+            // Créer le rôle
             builder.Entity<IdentityRole>().HasData(new IdentityRole()
             {
-                Id = "4ce0a981-6965-4a1e-9b36-f330ea2651f7",
+                Id = ROLE_ID,
                 Name = "admin",
                 NormalizedName = "admin".ToUpper(),
                 ConcurrencyStamp = "0609dc4a-a0c7-422b-98f1-a7f2fb6a23ce"
             });
-        }
 
-        private int IdentityRole()
-        {
-            throw new NotImplementedException();
+            // Créer le ApplicationUser
+            var appUser = new ApplicationUser
+            {
+                Id = USER_ADMIN_ID,
+                Email = "admin@test.com",
+                EmailConfirmed = true,
+                FirstName = "Admin",
+                LastName = "Du Site",
+                UserName = "admin",
+                NormalizedUserName = "admin".ToUpper()
+            };
+
+            // Créer le mot de passe
+            PasswordHasher<ApplicationUser> ph = new PasswordHasher<ApplicationUser>();
+            appUser.PasswordHash = ph.HashPassword(appUser, "Jaimelaprog1!");
+
+            // Ajouter l'utilisateur dans la BD
+            builder.Entity<ApplicationUser>().HasData(appUser);
+
+            // Ajouter les rôles de l'utilisateur.
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ROLE_ID,
+                UserId = USER_ADMIN_ID
+            });
         }
 
         public DbSet<Enseignant> Enseignants { get; set; }
--- DemoMVC_Identity/Models/ApplicationUser.cs
+++ DemoMVC_Identity/Models/ApplicationUser.cs
@@ -4,5 +4,8 @@ namespace DemoMVC_Identity.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
 }
